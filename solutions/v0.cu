6552e094f3e240ff4b486ca05d8f88c00329a87a
Baptiste <baptiste.mouillon21@gmail.com>
2024-11-19 10:25:40 +0100

    First commit

---
+++ solutions/v0.cu
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <iostream>
+#include <cuda_runtime.h>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
+
+__global__ void mykernel(float* r, const float* d, int n) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (i >= n || j >= n)
+        return;
+    float v = HUGE_VALF;
+    for (int k = 0; k < n; ++k) {
+        float x = d[n * i + k];
+        float y = d[n * k + j];
+        float z = x + y;
+        v = min(v, z);
+    }
+    r[n * i + j] = v;
+}
+
+static inline int divup(int a, int b) {
+    return (a + b - 1) / b;
+}
+
+void step(float* r, const float* d, int n) {
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, n * n * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(dGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(divup(n, dimBlock.x), divup(n, dimBlock.y));
+    mykernel << <dimGrid, dimBlock >> > (rGPU, dGPU, n);
+    CHECK(cudaGetLastError());
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
+
+int main() {
+    constexpr int n = 20000;
+    // Generate a random graph
+    vector<float> d(n * n);
+    srand(static_cast<unsigned>(time(nullptr)));
+    for (int i = 0; i < n * n; ++i) {
+        d[i] = static_cast<float>(rand()) / RAND_MAX;
+    }
+    // Compute shortest 2-edge paths
+    vector<float> r(n * n);
+    auto start = chrono::high_resolution_clock::now();
+    step(r.data(), d.data(), n);
+    auto end = chrono::high_resolution_clock::now();
+    // // Display results
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < n; ++j) {
+    //         cout << r[i * n + j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    chrono::duration<float> duration = end - start;
+    cout << "Time elapsed: " << duration.count() << " seconds\n";
+    return 0;
+}
---
+++ training/v2.cu
@@ -0,0 +1,105 @@
+#include <cstdlib>
+#include <iostream>
+#include <cuda_runtime.h>
+#include <chrono>
+#include <vector>
+
+using namespace std;
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
+
+__global__ void mykernel(float* r, const float* d, int n, int nn) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
+
+    // TO DO
+}
+
+__global__ void myppkernel(const float* r, float* d, int n, int nn) {
+    int ja = threadIdx.x;
+    int i = blockIdx.y;
+
+    float* t = d + nn * nn;
+
+    for (int jb = 0; jb < nn; jb += 64) {
+        int j = jb + ja;
+        float v = (i < n && j < n) ? r[n * i + j] : HUGE_VALF;
+        d[nn * i + j] = v;
+        t[nn * j + i] = v;
+    }
+}
+
+static inline int divup(int a, int b) {
+    return (a + b - 1) / b;
+}
+
+static inline int roundup(int a, int b) {
+    return divup(a, b) * b;
+}
+
+void step(float* r, const float* d, int n) {
+    int nn = roundup(n, 64);
+
+    // Allocate memory & copy data to GPU
+    float* dGPU = NULL;
+    CHECK(cudaMalloc((void**)&dGPU, 2 * nn * nn * sizeof(float)));
+    float* rGPU = NULL;
+    CHECK(cudaMalloc((void**)&rGPU, n * n * sizeof(float)));
+    CHECK(cudaMemcpy(rGPU, d, n * n * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Run kernel
+    {
+        dim3 dimBlock(64, 1);
+        dim3 dimGrid(1, nn);
+        myppkernel << <dimGrid, dimBlock >> > (rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Run kernel
+    {
+        dim3 dimBlock(8, 8);
+        dim3 dimGrid(nn / 64, nn / 64);
+        mykernel << <dimGrid, dimBlock >> > (rGPU, dGPU, n, nn);
+        CHECK(cudaGetLastError());
+    }
+
+    // Copy data back to CPU & release memory
+    CHECK(cudaMemcpy(r, rGPU, n * n * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(dGPU));
+    CHECK(cudaFree(rGPU));
+}
+
+int main() {
+    constexpr int n = 20000;
+    // Generate a random graph
+    vector<float> d(n * n);
+    srand(static_cast<unsigned>(time(nullptr)));
+    for (int i = 0; i < n * n; ++i) {
+        d[i] = static_cast<float>(rand()) / RAND_MAX;
+    }
+    // Compute shortest 2-edge paths
+    vector<float> r(n * n);
+    auto start = chrono::high_resolution_clock::now();
+    step(r.data(), d.data(), n);
+    auto end = chrono::high_resolution_clock::now();
+    // // Display results
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < n; ++j) {
+    //         cout << r[i * n + j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    chrono::duration<float> duration = end - start;
+    cout << "Time elapsed: " << duration.count() << " seconds\n";
+    return 0;
+}
